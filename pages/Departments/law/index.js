6276c238f42aecab2b06d7cbcce3d3932284cb5c
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-03-28 22:29:33 +0530

    jpg

--- pages/Departments/law/index.js
+++ pages/Departments/law/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import Image from "next/image";
 
 
-import img1 from '../images/law (1).jpg'
-import img2 from '../images/law (2).jpg'
-import img3 from '../images/law (3).jpg'
+import img1 from '../images/law (1).JPG'
+import img2 from '../images/law (2).JPG'
+import img3 from '../images/law (3).JPG'
 import img4 from '../images/law (4).jpg'
 import img5 from '../images/law (5).jpg'
 import img6 from '../images/law (6).jpg'
