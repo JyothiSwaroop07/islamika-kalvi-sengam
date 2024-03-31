e0ec03bd03990c4ff9c03b5f76f619d8c9b5a385
Amey Muke <amitjoshi2052003@gmail.com>
2024-03-31 21:05:02 +0530

    Done Favicon Changes

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/layout/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/layout/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/layout/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/layout/favicon/site.webmanifest"/>
+        <link rel="mask-icon" href="/layout/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+        <link rel='shortcut icon' href='/layout/favicon/favicon.ico' />
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
+        <meta
+          name='msapplication-config'
+          content='/layout/favicon/browserconfig.xml'
+        />
       </Head>
       <body className=' max-h-1'>
         <Main />
