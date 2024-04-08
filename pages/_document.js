1a3a35b1459fea4f1cb127aba24c8b4035d8343a
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-04-08 16:36:49 +0530

    static logo

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default function Document() {
         />
           <meta property="og:title" content="Islamiya Kalvi Sangam" />
           <meta property="og:description" content="GIVE HOPE THROUGH YOUR SADAQAH AND ZAKAT" />
-          <meta property="og:image" content="https://res.cloudinary.com/dchbfnlct/image/upload/v1711188558/fit_size_fit_xmjifp.png" />
+          <meta property="og:image" content="/public/layout/favicon/favicon.ico" />
           <meta property="og:url" content="https://www.islamiyakalvisangam.org/" />
           <meta property="og:type" content="website" />
       </Head>
