80cc5722cb9186ae7c2c337ad638004b11269ca2
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-04-08 14:40:32 +0530

    logo updation

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,9 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="180x180" href="/layout/favicon/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/layout/favicon/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/layout/favicon/favicon-16x16.png"/>
-        <link rel="manifest" href="/layout/favicon/site.webmanifest"/>
-        <link rel="mask-icon" href="/layout/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
-        <link rel='shortcut icon' href='/layout/favicon/favicon.ico' />
+        <link rel="manifest" href="https://res.cloudinary.com/dchbfnlct/image/upload/v1711188558/fit_size_fit_xmjifp.png"/>
+        <link rel="mask-icon" href="https://res.cloudinary.com/dchbfnlct/image/upload/v1711188558/fit_size_fit_xmjifp.png" color="#5bbad5"/>
+        <link rel='shortcut icon' href='https://res.cloudinary.com/dchbfnlct/image/upload/v1711188558/fit_size_fit_xmjifp.png' />
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
         <meta
