7b9c403cc31daa6c7bb2193f3fdc3d291fb99ecc
chandusiriyala <chandusiriyala116@gmail.com>
2024-09-12 00:52:17 +0530

    build errors rectified

--- pages/RejectWaqf/index.js
+++ pages/RejectWaqf/index.js
@@ -13,25 +13,31 @@ const RejectWaqf = () => {
                 </header>
                 <section style={styles.description}>
                     <p style={styles.paragraph}>
-                        <span style={styles.highlight}>Save Indian Waqf Properties, Reject The Waqf Amendment Bill!</span>
+                        <span style={styles.highlight}>
+                            Save Indian Waqf Properties, Reject The Waqf Amendment Bill!
+                        </span>
                         <br /><br />
                         Let’s stand together to defend the sanctity of Waqf and ensure its preservation for future generations.
                         <br /><br />
                         Abu Bakr narrated that he heard the messenger of Allah (pbuh) say, 
-                        <span style={styles.italic}>"If people see some evil but do not change it, soon Allah will send His punishment upon them all”</span> 
+                        <span style={styles.italic}>
+                            &quot;If people see some evil but do not change it, soon Allah will send His punishment upon them all&quot;
+                        </span> 
                         – Sunan Ibn Maajah Vol 5, Hadeeth No. 4005
                         <br /><br />
                         This is an urgent call to the Muslims in India to stop this evil which breaches the sacred status of waqf, 
                         and has evil repercussions on the future of Islaamic institutions. 
-                        We will bear Allah’s wrath and the curse of subsequent generations, if we allow this bill to pass.
+                        We will bear Allah’s wrath and the curse of subsequent generations if we allow this bill to pass.
                         <br /><br />
-                        <span style={styles.bold}>Stop the evil or bear its liabilities in this life and in the Hereafter!</span> 
+                        <span style={styles.bold}>
+                            Stop the evil or bear its liabilities in this life and in the Hereafter!
+                        </span> 
                         Say no to the Waqf Amendment Bill!
                         <br /><br />
                         At least 5 million Muslims of India should send their rejection of the Waqf Amendment Bill. 
                         As Muslims of India, we will be held accountable if we do not prevent the Muslim Waqf properties from being taken away from the Ummah.
                         <br /><br />
-                        Send your Rejection by Scanning this QR Code. You can send your rejection to Waqf Bill to Joint Parliamentary Committee.
+                        Send your Rejection by scanning this QR Code. You can send your rejection to Waqf Bill to Joint Parliamentary Committee.
                         <br /><br />
                         <a href="https://waqfbill2024.com" target="_blank" rel="noopener noreferrer" style={styles.link}>
                             Visit https://waqfbill2024.com
